6370df87d3b1b69996eeccddc25911a60eb3e91e
Nils Martel <nils.martel@graphmasters.net>
2020-02-15 11:43:20 +0100

    New Sample File

--- lola/src/main.rs
+++ lola/src/main.rs
@@ -1,3 +1,24 @@
+struct Identifier(String);
+struct Function {
+    name: Identifier,
+    parameters: Vec<Identifier>,
+    expression: Expr,
+}
+
+enum Expr {
+    Const(bool),
+    Var(Identifier),
+    Not(Box<Expr>),
+    Func { ident: Identifier, param: Vec<Expr> },
+    And(Box<Expr>, Box<Expr>),
+    Or(Box<Expr>, Box<Expr>),
+}
+
+// a | b
+// a & b
+// !a
+// func(a)
+
 fn main() {
     println!("Hello, world!");
 }
